808ca75344c338df824f784d4ba5838d4dfd0e72
Heisenberg <omaradelawad20013@gmail.com>
2023-10-03 23:23:05 +0300

    created a navLinks data object

---
+++ src/data/navLinks.js
@@ -0,0 +1,7 @@
+export const navLinks = [
+  { id: 1, label: "قرآن", path: "/" },
+  { id: 2, label: "الأحاديث", path: "/hadith" },
+  { id: 3, label: "مواعيد الصلاة", path: "/pray" },
+  { id: 4, label: "حصن المسلم", path: "/hisn" },
+  { id: 5, label: "المفضلة", path: "/favorites" },
+];
