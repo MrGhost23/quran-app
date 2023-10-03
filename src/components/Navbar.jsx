457e3b07fb769ea08b472eaa36a089171c8a2c3d
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-10-03 22:07:06 +0300

    Navlink to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Fragment, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { links } from "../utils/links";
 import { FaBars } from "react-icons/fa6";
 
@@ -9,20 +9,63 @@ const Navbar = () => {
     <header className="bg-white pb-7">
       <div className="container mx-auto px-4 py-6 flex items-center">
         <div className="ml-auto md:w-48 flex-shrink-0">
-          <p className="text-green-600 text-3xl font-bold">قرآن</p>
+          <Link className="text-green-600 text-3xl font-bold" to="/">
+            قرآن
+          </Link>
         </div>
 
         <nav className="hidden lg:contents">
           <ul className="ml-auto flex items-center justify-center font-semibold text-xl">
-            <li className="ml-2 lg:ml-4 relative inline-block text-green-600">
-              قرآن
-            </li>
-            <li className="ml-2 lg:ml-4 relative inline-block">الأحاديث</li>
             <li className="ml-2 lg:ml-4 relative inline-block">
-              مواعيد الصلاة
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "text-green-600" : "text-gray-400"
+                }
+              >
+                قرآن
+              </NavLink>
             </li>
-            <li className="ml-2 lg:ml-4 relative inline-block">حصن المسلم</li>
-            <li className="ml-2 lg:ml-4 relative inline-block">المفضلة</li>
+            <li className="ml-2 lg:ml-4 relative inline-block">
+              <NavLink
+                to="/hadith"
+                className={({ isActive }) =>
+                  isActive ? "text-green-600" : "text-gray-400"
+                }
+              >
+                أحاديث
+              </NavLink>
+            </li>
+            <li className="ml-2 lg:ml-4 relative inline-block">
+              <NavLink
+                to="/pray"
+                className={({ isActive }) =>
+                  isActive ? "text-green-600" : "text-gray-400"
+                }
+              >
+                مواعيد الصلاة
+              </NavLink>
+            </li>
+            <li className="ml-2 lg:ml-4 relative inline-block">
+              <NavLink
+                to="/hisn"
+                className={({ isActive }) =>
+                  isActive ? "text-green-600" : "text-gray-400"
+                }
+              >
+                حصن المسلم
+              </NavLink>
+            </li>
+            <li className="ml-2 lg:ml-4 relative inline-block">
+              <NavLink
+                to="/favorites"
+                className={({ isActive }) =>
+                  isActive ? "text-green-600" : "text-gray-400"
+                }
+              >
+                المفضلة{" "}
+              </NavLink>
+            </li>
           </ul>
         </nav>
         <li className="lg:hidden flex">
