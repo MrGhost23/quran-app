a800b207a73a2e4aea3d53669b889a8f50957b62
Heisenberg <omaradelawad20013@gmail.com>
2023-10-04 00:28:05 +0300

    added className prop to the wrapper component

--- src/ui/Wrapper.jsx
+++ src/ui/Wrapper.jsx
@@ -1,5 +1,9 @@
-const Wrapper = ({ children }) => {
-  return <div className="px-3 py-5 bg-[#ECF0F4] rounded-lg">{children}</div>;
+const Wrapper = ({ children, className }) => {
+  return (
+    <div className={`px-3 py-6 bg-[#ECF0F4] rounded-lg ${className || ""}`}>
+      {children}
+    </div>
+  );
 };
 
 export default Wrapper;
