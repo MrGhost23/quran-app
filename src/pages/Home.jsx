a7495d78e0e99739903acfa41beb340d093c3774
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-10-03 04:10:08 +0300

    delete comments and make loading inside reciters div

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { FaHeart } from "react-icons/fa6";
 
 const Home = () => {
   const [reciters, setReciters] = useState([]);
@@ -20,23 +21,34 @@ const Home = () => {
       }
     };
 
-    // Call the fetchReciters function when the component mounts
     fetchReciters();
-  }, []); // The empty dependency array ensures this effect runs only once
+  }, []);
   return (
     <div>
-      <h1>List of Reciters</h1>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <ul>
-          {reciters.map((reciter) => (
-            <Link key={reciter.id} to={`/${reciter.id}`}>
-              <li>{reciter.name}</li>
-            </Link>
-          ))}
-        </ul>
-      )}
+      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-4 pt-3 h-0 py-8">
+        {loading ? (
+          <div className="">Loading</div>
+        ) : (
+          reciters.map((reciter) => (
+            <div
+              className="flex bg-white flex-grow justify-between p-2 rounded-2xl py-4 items-center"
+              key={reciter.id}
+            >
+              <div className="number bg-green-200 text-green-600 p-1 rounded-full">
+                {reciter.id}
+              </div>
+              <h2 className="text-lg cursor-pointer font-medium">
+                {reciter.name}
+              </h2>
+              <div className="fav-icon">
+                <div className="cursor-pointer text-lg text-green-600">
+                  <FaHeart />
+                </div>
+              </div>
+            </div>
+          ))
+        )}
+      </ul>
     </div>
   );
 };
