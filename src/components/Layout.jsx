2a1b97fcf476160fb64976a43c25000b20e6051d
Heisenberg <omaradelawad20013@gmail.com>
2023-10-03 21:07:53 +0300

    updated layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <div className="flex flex-col-reverse lg:grid grid-cols-4 gap-4 px-4 min-h-[calc(80vh-112px)]">
-      <div className="col-span-3 bg-[#ECF0F4] overflow-y-auto rounded-lg min-h-[calc(100vh-112px)]">
+      <div className="col-span-3">
         <Outlet />
       </div>
       <div className="col-span-1 md:h-auto">right side</div>
