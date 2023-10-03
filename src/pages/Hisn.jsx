ed1e1fde2ead40512bc89391fb2ab86e5f138697
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-10-03 23:56:18 +0300

    Wrapper instead of Card

--- src/pages/Hisn.jsx
+++ src/pages/Hisn.jsx
@@ -1,13 +1,13 @@
-import Card from "../ui/Card";
+import Wrapper from "../ui/Wrapper";
 import ScrollableDiv from "../ui/ScrollableDiv";
 
 const Hisn = () => {
   return (
-    <Card>
+    <Wrapper>
       <ScrollableDiv className="grid grid-cols-1 md:grid-cols-2 h-[calc(90vh-112px)] max-h-[calc(90vh-112px)] lg:grid-cols-3 gap-4">
         <div>Hisn</div>
       </ScrollableDiv>
-    </Card>
+    </Wrapper>
   );
 };
 export default Hisn;
