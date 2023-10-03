f331a3af702864bafba5936dc6aa93a4d622c2fa
Heisenberg <omaradelawad20013@gmail.com>
2023-10-04 00:18:43 +0300

    added search bar in the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import { FaHeart } from "react-icons/fa6";
 import ScrollableDiv from "../ui/ScrollableDiv";
 import Card from "../ui/Card";
-import Search from "../ui/Search";
+import SearchBar from "../ui/SearchBar";
 
 const Home = () => {
   const [reciters, setReciters] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+
   useEffect(() => {
     const fetchReciters = async () => {
       try {
@@ -27,13 +28,19 @@ const Home = () => {
     fetchReciters();
   }, []);
   return (
-    <Card>
+    <Card className="flex flex-col gap-6">
+      <SearchBar
+        placeholder="ابحث عن قارئ.."
+        value={searchValue}
+        onSearch={(e) => setSearchValue(e.target.value)}
+        containerClass="max-w-sm"
+        inputClass="h-10 rounded-md"
+      />
       <ScrollableDiv className="grid grid-cols-1 md:grid-cols-2 h-[calc(90vh-112px)] max-h-[calc(90vh-112px)] lg:grid-cols-3 gap-4">
         {loading ? (
           <div className="">Loading</div>
         ) : (
           <>
-            <div className="w-full mb-4">Search</div>
             {reciters.map((reciter) => (
               <div
                 className="flex bg-white flex-grow justify-between p-2 rounded-2xl py-4 items-center"
