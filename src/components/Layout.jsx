075bd5044ddb11628f3633ef6336b6cb988d5558
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-10-03 04:22:18 +0300

    fixed right side in mobile - almost. not final result

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="flex flex-col-reverse lg:grid grid-cols-4 gap-4 px-4 min-h-[calc(90vh-112px)]">
-      <div className="col-span-3 bg-[#ECF0F4] overflow-y-auto rounded-lg max-h-[calc(100vh-112px)]">
+    <div className="flex flex-col-reverse lg:grid grid-cols-4 gap-4 px-4 min-h-[calc(80vh-112px)]">
+      <div className="col-span-3 bg-[#ECF0F4] overflow-y-auto rounded-lg min-h-[calc(100vh-112px)]">
         <Outlet />
       </div>
       <div className="col-span-1 md:h-auto">right side</div>
