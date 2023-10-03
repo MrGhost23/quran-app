c470c7379847f19edbe0c22ea372786fc2faadc6
Heisenberg <omaradelawad20013@gmail.com>
2023-10-04 00:10:13 +0300

    added className prop to the card ui component

--- src/ui/Card.jsx
+++ src/ui/Card.jsx
@@ -1,5 +1,9 @@
-const Card = ({ children }) => {
-  return <div className="px-3 py-5 bg-[#ECF0F4] rounded-lg">{children}</div>;
+const Card = ({ children, className }) => {
+  return (
+    <div className={`px-3 py-5 bg-[#ECF0F4] rounded-lg ${className || ""}`}>
+      {children}
+    </div>
+  );
 };
 
 export default Card;
